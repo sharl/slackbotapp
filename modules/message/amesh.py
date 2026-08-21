a9d147244f3f5ea000d8c0f3dbd6b4c808fa3e42
Sharl Morlaroll <sharl@hauN.org>
2026-08-21 14:59:40 +0900

    do not regenerate within 5 minutes

--- modules/message/amesh.py
+++ modules/message/amesh.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 import subprocess
+import time
 
-from modules import uploadFile
+from redis import Redis
+
+from modules import postMessage, uploadFile
+
+
+KEY_PREFIX = 'amesh'
+EXPIRES = 60 * 5
 
 
 class call:
@@ -14,6 +21,32 @@ class call:
 
         keyword = 'アメッシュ'
         if text == keyword and item.get('bot_id') is None:
+            r = Redis(decode_responses=True)
+            key = f'{KEY_PREFIX}.{caches.channel_ids.get(channel, 'dummy')}'
+            pre = r.get(key)
+            # すでに出力していたら無視
+            if pre:
+                before = int(time.time() - float(pre))
+                message = ''
+                if before < 60:
+                    message = f'{before}秒くらい前に'
+                else:
+                    message = f'{int(before / 60)}分くらい前に'
+
+                postMessage(
+                    client,
+                    keyword,
+                    caches.icon_emoji,
+                    channel,
+                    message,
+                    thread_ts=thread_ts,
+                )
+
+                return
+            else:
+                r.set(key, time.time())         # store: str
+                r.expire(key, EXPIRES)
+
             amesh = subprocess.check_output(['amesh', '-c'])
             with open('/tmp/amesh.png', 'wb') as fd:
                 fd.write(amesh)
